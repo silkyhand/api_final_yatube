8ff875bd1efe031f26cbb0676c0e13f5aa0d0e87
Владимир Шалдыкин <shaldykin@gmail.com>
2022-02-24 22:53:43 +0300

    adding models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -16,6 +16,15 @@ class Post(models.Model):
         return self.text
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title        
+
+
 class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
@@ -24,3 +33,24 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
+
+    
+    def __str__(self):
+        return f'{self.user} follows {self.following}'
+
