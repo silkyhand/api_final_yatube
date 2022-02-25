b77cd3fe3e7fc50a857c1c38e781c8f229930a38
Владимир Шалдыкин <shaldykin@gmail.com>
2022-02-25 16:39:48 +0300

    after working at work

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,18 +2,27 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Post."""
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Post
 
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group."""
+    class Meta:
+        fields = '__all__'
+        model = Group
+        
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comment."""
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
@@ -21,3 +30,12 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Follow."""
+    class Meta:
+        fields = '__all__'
+        model = Follow
+
+
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,50 @@
 # TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from posts.models import Group, Post
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer, FollowSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Post."""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для модели Group."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_class = [IsAuthenticated]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Comment."""
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        new_queryset = post.comments.all()
+        return new_queryset
+
+class FollowViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Follow."""
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        posts = Post.objects.filter(author__following__user=self.request.user)
+        new_queryset = posts
+        return new_queryset
